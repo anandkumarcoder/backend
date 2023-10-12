ab8ed072f6e10990c476af49d667ed247114e854
Anand <akanand104@gmail.com>
2023-10-13 00:34:16 +0530

    first episode completed

---
+++ 02deployment/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 5000;
+
+//  "/" is home route
+
+app.get("/", (req, res) => {
+  res.send("Hello World! war");
+});
+// ------------
+
+
+app.get('/twitter', (req, res) =>{
+res.send('@Anand5')
+})
+
+
+app.get('/login', (req, res) =>{
+    res.send('<h1>Hi please visit twitter login page <a>https://twitter.com/i/flow/login</a></h1>')
+    })
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
